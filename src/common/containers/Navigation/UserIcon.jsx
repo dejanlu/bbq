8986a2874f091a2e81de635829dfc9c1981838ad
dejanlu <dejan.bl@gmail.com>
2020-08-18 19:25:28 +0200

    Quickfix - remove unused var-s

--- src/common/containers/Navigation/UserIcon.jsx
+++ src/common/containers/Navigation/UserIcon.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 
-import { RiUserAddLine, RiUserLine } from "react-icons/ri";
+import { RiUserAddLine } from "react-icons/ri";
 import { Link } from "react-router-dom";
+
+// icon for logged in user RiUserLine
 
 const UserIcon = () => {
   return (
