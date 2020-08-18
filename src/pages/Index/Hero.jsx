b71567c948e717c658ede289a2857817e310c383
dejanlu <dejan.bl@gmail.com>
2020-08-18 14:18:07 +0200

    Add typography styles
    Add Hero messages and styles

--- src/pages/Index/Hero.jsx
+++ src/pages/Index/Hero.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 
 const Hero = () => {
-  return <div className="hero"></div>;
+  return (
+    <div className="hero">
+      <h1 className="hero__heading">Stari recepti u novom vremenu!</h1>
+      <h3 className="hero__text">Dostavljamo na vašu adresu:</h3>
+    </div>
+  );
 };
 
 export default Hero;
